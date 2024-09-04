999c58d024b5ab4222cc20ae5135288446987ad6
malangzo <yooj1124@icloud.com>
2024-09-05 01:09:58 +0900

    modify

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -7,9 +7,9 @@ def hanoi(n, first, second, thrid):
         print(first, thrid)
     
     else: # 원판을 n개(<= 20) 옮기는 경우
-        hanoi(n-1, first, thrid, second) # 세 번째 장대를 보조 장대로 사용해 두 번째 장대로 n-1개 원판 이동
-        print(first, thrid) # 과정 출력
-        hanoi(n-1, second, first, thrid) # 첫 번째 장대를 보조 장대로 사용해 세 번째 장대로 n-1개 원판 이동
+        hanoi(n-1, first, thrid, second) # 첫 번째에서 두 번째 장대로 n-1개 원판 이동
+        print(first, thrid) # n번째 원판을 첫 번째에서 세 번째 장대로 이동
+        hanoi(n-1, second, first, thrid) # 두 번째에서 세 번째 장대로  n-1개 원판 이동
         
 n = int(input())
 
