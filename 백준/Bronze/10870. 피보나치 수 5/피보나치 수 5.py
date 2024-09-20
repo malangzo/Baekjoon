e986dec989390dbf05c4f9c5583d52aebf069756
Yoojeong Jang <113616278+malangzo@users.noreply.github.com>
2024-09-20 23:56:01 +0900

    Update 피보나치 수 5.py

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -2,13 +2,13 @@
 
 num = int(input())
 
-i, j = 0, 1
+i, j = 0, 1 # 0번째, 1번째 피보나치 수 정의
 
-if num <= 1:
+if num <= 1: # 0번째, 1번째일 경우 그대로 출력
     print(num)
 
 else:
-    for _ in range(2, num + 1):
+    for _ in range(2, num + 1): # 2번째 피보나치 수부터 i와 j를 더해 피보나치 수 구함
         k = i + j
         i, j = j, k
     print(k)
